0f0ecc417a05e878887873f4aef3535fe40a9c0e
Michel Many <michel@nitdesign.com.br>
2016-04-19 20:35:06 -0300

    atualizando migration

---
+++ database/migrations/2016_04_19_231731_create_table_banners_002.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableBanners002 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('banners', function (Blueprint $table) {
+            $table->integer('order')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
